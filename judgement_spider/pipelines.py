d4bae60a3b00d50bc42cfce23bd3bccd356cfcbc
stack <whyaiwxf@gmail.com>
2018-11-23 23:21:18 +0800

    按照日期存入数据

--- judgement_spider/pipelines.py
+++ judgement_spider/pipelines.py
@@ -5,8 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
-import base64
-from .util.toolbox import current_time, current_time_milli
+
+from judgement_spider.util.toolbox import current_time_milli, current_date
 
 
 class JudgementSpiderPipeline(object):
@@ -17,7 +17,7 @@ class JudgementSpiderPipeline(object):
         # self.docs = self.db['docs']
         self.client = MongoClient(host, int(port))
         self.db = self.client[db]
-        self.collection = self.db[collection]
+        # self.collection = self.db[collection]
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -45,8 +45,8 @@ class JudgementSpiderPipeline(object):
         # item['name'] = base64.b64encode(item['name'])
         # item['court'] = base64.b64encode(item['court'])
         item['crawled_at'] = str(current_time_milli())
-        self.__persist(item)
+        self.__persist(item, current_date())
         return item
 
-    def __persist(self, item):
-        self.collection.insert_one(item)
+    def __persist(self, item, collection_name):
+        self.db[collection_name].insert_one(item)
