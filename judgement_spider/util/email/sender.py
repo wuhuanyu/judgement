f31a016ff2341f1123d38610d1f542d9001f6471
吴环宇 <1790949749@qq.com>
2018-12-07 11:04:08 +0800

    email 配置由文件提供

--- judgement_spider/util/email/sender.py
+++ judgement_spider/util/email/sender.py
@@ -2,19 +2,39 @@
 import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
+from judgement_spider.util.toolbox import load_json
+from pathlib import Path
 class Sender():
+    def __init__(self,config_path):
+        if not Path(config_path).is_file:
+            raise Exception('Please provide your email config')
+        self.config:dict=load_json(config_path)
     def send_email(self, subject, content):
-        email_client = smtplib.SMTP('smtp.163.com',25)
-        email_client.login('18852856450@163.com', 'why90951213')
+        for key in ['smtp_server','port','user_name','passwd','from','to']:
+            if key not in list(self.config.keys()):
+                raise Exception('Please check your config file')
+        
+        config=self.config
+
+        smtp_server=config['smtp_server']
+        port=int(config['port'])
+        user_name=config['user_name']
+        passwd=config['passwd']
+        from_=config['from']
+        to_=config['to']
+
+        email_client = smtplib.SMTP(smtp_server,port)
+        email_client.login(user_name, passwd)
         msg = MIMEText(content, 'plain', 'utf-8')
         msg['Subject'] = Header(subject, 'utf-8')  # subject
-        msg['From'] = '18852856450@163.com'
-        msg['To'] = '18852856450@163.com'
-        email_client.sendmail('18852856450@163.com', '18852856450@163.com', msg.as_string())
+        msg['From'] = from_
+        msg['To'] = to_
+        email_client.sendmail(from_, to_, msg.as_string())
         email_client.quit()
 
 
 if __name__ == '__main__':
-    sender=Sender()
+    pass
+    sender=Sender('/Users/stack/code/py3/wenshu/judgement_spider/.email.json')
     sender.send_email('log','hello,world')
 
